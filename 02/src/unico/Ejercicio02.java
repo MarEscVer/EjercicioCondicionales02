f9726a5570af8fef0ad040326fee64597a89c85d
Maria <maria_escribanoverde@gmail.com>
2022-11-07 16:37:30 +0100

    FINAL

--- 02/src/unico/Ejercicio02.java
+++ 02/src/unico/Ejercicio02.java
@@ -6,7 +6,11 @@ public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		
+/*Realiza un programa que pida una hora por teclado y que luego muestre "buenos días",
+ * "buenas tardes" o "buenas noches" según la hora. Se utilizarán los tramos de 6 a 12,
+ * de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en cuenta las horas, los minutos
+ * no se deben introducir por teclado.
+ */
 		final String DIA = "Buenos días";
 		final String TARDE = "Buenas tardes";
 		final String NOCHE = "Buenas noches";
